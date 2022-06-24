73836f3585fe3246282afcfefc0c56c7a1b2a1f0
leonardtkt <leonard@tkt.paris>
2022-06-24 17:43:59 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  Text,
+  StyleSheet,
+  
+} from 'react-native';
+
+
+const App = ({children, title}) => {
+
+  return (
+    <Text>Hello World!</Text>
+  )
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
