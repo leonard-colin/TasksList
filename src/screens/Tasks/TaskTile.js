c2a463ca06ab95a6e2a912059c8c075c6031bc39
leonardtkt <leonard@tkt.paris>
2022-06-27 18:36:43 +0200

    Finalize app

--- src/screens/Tasks/TaskTile.js
+++ src/screens/Tasks/TaskTile.js
@@ -1,20 +1,31 @@
 import React from 'react'
-import { Text, Image, StyleSheet, View } from 'react-native'
+import { Text, Image, StyleSheet, View, Pressable } from 'react-native'
 
-export default function TaskTile({task}) {
+export default function TaskTile({ task, onUpdateTask, onDeleteTask }) {
+  const onChangeStatus = () => {
+    onUpdateTask(task.id)
+  }
+
+  const _onDeleteTask = () => {
+    onDeleteTask(task.id)
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.subContainer}>
+      <Pressable onPress={onChangeStatus} style={styles.subContainer}>
         <Image 
           style={styles.check}
-          source={require('../../../assets/icons/circle.png')}
+          source={task.isCompleted ? require('../../../assets/icons/check_circle.png') : require('../../../assets/icons/circle.png')}
         />
         <Text style={styles.title}>{task.title}</Text>
-      </View>
-      <Image 
+      </Pressable>
+
+      <Pressable onPress={_onDeleteTask}>
+        <Image 
         style={styles.check}
         source={require('../../../assets/icons/delete.png')}
-      />
+        />
+      </Pressable>
     </View>
     
   )
--- src/screens/Tasks/index.js
+++ src/screens/Tasks/index.js
@@ -1,38 +1,103 @@
 import React, {useState} from 'react'
-import { Text, FlatList } from 'react-native'
+import { FlatList, View, StyleSheet } from 'react-native'
 
 import Header from '../../components/Header'
 import TaskForm from './TaskForm'
 import TaskTile from './TaskTile'
+import FloatingBtn from '../../components/FloatingBtn'
+import Counter from '../../components/Counter'
 
 export default function TasksScreen() {
+  const [isFormVisible, setIsFormVisible] = useState(false)
   // Liste de tâches
   // State pour garder en mémoire les tâches.
-  const [tasks, setTasks] = useState([
-    {title: "Hello world!", isCompleted: false},
-    {title: "Once again, Hello! ", isCompleted: false}
-  ])
+  const [tasks, setTasks] = useState([])
 
   // item = {title: "Hello world!", isCompleted: false}
   const renderItem = ({item}) => {
-    return <TaskTile task={item}/>
+    return <TaskTile task={item} onUpdateTask={onUpdateTask} onDeleteTask={onDeleteTask} />
   }
-  // asyncStorage si on veut garder l'état dans la mémoire du téléphone
+  // TIPS: asyncStorage si on veut garder l'état dans la mémoire du téléphone
+
+  // Ajouter une fonction pour ajouter une tâche au state
+  // Passer cette fonction à notre formulaire
+  const onAddTask = (title) => {
+    setTasks([...tasks, {
+      id: Date.now(),
+      title,
+      isCompleted: false
+    }])
+  }
+
+  const onUpdateTask = (id) => {
+    let newTasks = []
+
+    tasks.forEach(t => {
+      if (t.id !== id) {
+        newTasks.push(t)
+        return
+      }
+
+      newTasks.push({
+        id,
+        title: t.title,
+        isCompleted: !t.isCompleted
+      })
+    })
+
+    setTasks(newTasks)
+  }
+
+  const onDeleteTask = (id) => {
+    let newTasks = []
+
+    tasks.forEach(t => {
+      if (t.id !== id) {
+        newTasks.push(t)
+      } 
+    })
+
+    setTasks(newTasks)
+  }
+
+  const _toggleForm = () => {
+    setIsFormVisible(!isFormVisible)
+  }
  
-  // 2 TasksCounter => props nb & title
+  // 2x TasksCounter => props nb & title
   // TasksList => return FlatList => TaskTile
+
+  // Ajouter un bouton flottant => style absolute
+  // / callback => rendu conditionnel Taskform
   return (
-    <FlatList
-      ListHeaderComponent={
-        <>
-        <Header />
-        <TaskForm /> 
-        </>
-    }
-      contentContainerStyle={{flex:1}}
-      data={tasks}
-      keyExtractor={(item, index) => index.toString()}
-      renderItem={renderItem}
-    />
+    <>
+      <FlatList
+        ListHeaderComponent={
+          <>
+          <Header />
+          {isFormVisible && <TaskForm onAddTask={onAddTask} />}
+          <View style={styles.container}>
+            <Counter nb={tasks.length} title="Tâches crées"/>
+            <Counter nb={tasks.filter(t => t.isCompleted === true).length } title="Tâches effectuées"/>
+          </View>  
+          </>
+      }
+        contentContainerStyle={{flex:1}}
+        data={tasks}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderItem}
+      />
+      <FloatingBtn toggle={_toggleForm} isOpen={isFormVisible} />
+    </>
+    
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 30,
+    paddingHorizontal: 20    
+  }
+})
