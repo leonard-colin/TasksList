2612ba08fc7168c8e377e9e9b8e91466af5287b9
leonardtkt <leonard@tkt.paris>
2022-06-27 14:44:41 +0200

    FlatList

--- src/screens/Tasks/index.js
+++ src/screens/Tasks/index.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Text, FlatList } from 'react-native'
+
 import Header from '../../components/Header'
+import TaskTile from './TaskTile'
 
 export default function TasksScreen() {
+  // Liste de tâches
+  // State pour garder en mémoire les tâches.
+  const [tasks, setTasks] = useState([
+    {title: "Hello world!", isCompleted: false},
+    {title: "Once again, Hello! ", isCompleted: false}
+  ])
+
+  // item = {title: "Hello world!", isCompleted: false}
+  const renderItem = ({item}) => {
+    return <TaskTile task={item}/>
+  }
+  // asyncStorage si on veut garder l'état dans la mémoire du téléphone
+ 
+  // 2 TasksCounter => props nb & title
+  // TasksList => return FlatList => TaskTile
   return (
-    <>
-    <Header />
-    </>
+    <FlatList
+      ListHeaderComponent={<Header />}
+      contentContainerStyle={{flex:1}}
+      data={tasks}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={renderItem}
+    />
   )
 }
